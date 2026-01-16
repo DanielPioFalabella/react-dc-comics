358db58edee097e48075a2648fbff8f606079138
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2026-01-16 16:47:15 +0100

    style header + main + footer

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -4,52 +4,62 @@ const Footer = () => {
             <div className="centratura-contenuto footer-container">
                 <div className="footer-text">
                     {/* DC comics */}
-                    <h3>DC COMICS</h3>
-                    <a href="#">Characters</a>
-                    <a href="#">Comics</a>
-                    <a href="#">Movies</a>
-                    <a href="#">TV</a>
-                    <a href="#">Games</a>
-                    <a href="#">Videos</a>
-                    <a href="#">News</a>
-                    {/* shop */}
-                    <h3>SHOP</h3>
-                    <a href="#">Shop DC</a>
-                    <a href="#">Shop DC Collectibles</a>
-                    {/* DC */}
-                    <h3>DC</h3>
-                    <a href="#">Terms Of Use</a>
-                    <a href="#">Privacy policy (New)</a>
-                    <a href="#">Ad Choices</a>
-                    <a href="#">Advertising</a>
-                    <a href="#">Jobs</a>
-                    <a href="#">Subscriptions</a>
-                    <a href="#">Talent Workshops</a>
-                    <a href="#">CPSC Certificates</a>
-                    <a href="#">Ratings</a>
-                    <a href="#">Shop Help</a>
-                    <a href="#">Contact Us</a>
-                    {/* sites */}
-                    <h3>SITES</h3>
-                    <a href="#">DC</a>
-                    <a href="#">MAD Magazine</a>
-                    <a href="#">DC Kids</a>
-                    <a href="#">DC Universe</a>
-                    <a href="#">DC Power Visa</a>
+                    <div className="footer-text-col">
+                        <h3>DC COMICS</h3>
+                        <a href="#">Characters</a>
+                        <a href="#">Comics</a>
+                        <a href="#">Movies</a>
+                        <a href="#">TV</a>
+                        <a href="#">Games</a>
+                        <a href="#">Videos</a>
+                        <a href="#">News</a>
+                        {/* shop */}
+                        <h3>SHOP</h3>
+                        <a href="#">Shop DC</a>
+                        <a href="#">Shop DC Collectibles</a>
+                    </div>
+                    <div className="footer-text-col">
+                        {/* DC */}
+                        <h3>DC</h3>
+                        <a href="#">Terms Of Use</a>
+                        <a href="#">Privacy policy (New)</a>
+                        <a href="#">Ad Choices</a>
+                        <a href="#">Advertising</a>
+                        <a href="#">Jobs</a>
+                        <a href="#">Subscriptions</a>
+                        <a href="#">Talent Workshops</a>
+                        <a href="#">CPSC Certificates</a>
+                        <a href="#">Ratings</a>
+                        <a href="#">Shop Help</a>
+                        <a href="#">Contact Us</a>
+                    </div>
+                    <div className="footer-text-col">
+                        {/* sites */}
+                        <h3>SITES</h3>
+                        <a href="#">DC</a>
+                        <a href="#">MAD Magazine</a>
+                        <a href="#">DC Kids</a>
+                        <a href="#">DC Universe</a>
+                        <a href="#">DC Power Visa</a>
+                    </div>
                 </div>
                 <div className="footer-img">
                     <img src="./assets/img/dc-logo-bg.png" alt="background con logo DC" />
                 </div>
             </div>
-            <div className="CTA-social">
+            <div className="bg-gray">
                 <div className="centratura-contenuto">
-                <button>SIGN-UP NOW!</button>
-                <h3>FOLLOW US</h3>
-                <img src="./assets/img/footer-facebook.png" alt="Facebook" />
-                <img src="./assets/img/footer-twitter.png" alt="Twitter" />
-                <img src="./assets/img/footer-youtube.png" alt="YouTube" />
-                <img src="./assets/img/footer-pinterest.png" alt="Pinteres" />
-                <img src="./assets/img/footer-periscope.png" alt="Periscope" />
+                    <div className="CTA-social">
+                        <button>SIGN-UP NOW!</button>
+                        <div className="CTA-social-dx">
+                            <h3>FOLLOW US</h3>
+                            <img src="./assets/img/footer-facebook.png" alt="Facebook" />
+                            <img src="./assets/img/footer-twitter.png" alt="Twitter" />
+                            <img src="./assets/img/footer-youtube.png" alt="YouTube" />
+                            <img src="./assets/img/footer-pinterest.png" alt="Pinteres" />
+                            <img src="./assets/img/footer-periscope.png" alt="Periscope" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </footer>
--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -1,8 +1,30 @@
 const Main = () => {
     return (
         <main>
-            <div className="centratura-contenuto">
+            <div className="fascia-nera centratura-contenuto">
                 <p>Content goes here</p>
+            </div>
+            <div className="fascia-azzurra">
+                <div className="fascia-azzurra-content">
+                    <img src="./assets/img/buy-comics-digital-comics.png" alt="" />
+                    <a href="#">DIGITAL COMICS</a>
+                </div>
+                <div className="fascia-azzurra-content">
+                    <img src="./assets/img/buy-comics-merchandise.png" alt="" />
+                    <a href="#">DC MERCHANDISE</a>
+                </div>
+                <div className="fascia-azzurra-content">
+                    <img src="./assets/img/buy-comics-subscriptions.png" alt="" />
+                    <a href="#">SUBSCRIPTION</a>
+                </div>
+                <div className="fascia-azzurra-content">
+                    <img src="./assets/img/buy-comics-shop-locator.png" alt="" />
+                    <a href="#">COMIC SHOP LOCATOR</a>
+                </div>
+                <div className="fascia-azzurra-content">
+                    <img src="./assets/img/buy-dc-power-visa.svg" alt="" />
+                <a href="#">DC POWER VISA</a>
+                </div>
             </div>
         </main>
     )
