e5fbb2ab197768972d31b4d9435177cfdb1e4983
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2026-01-16 15:33:53 +0100

    header + main + footer

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,23 @@
+const Header = () => {
+    return (
+        <header>
+            <figure>
+                <img src="./public/dc-logo.png" alt="logo" />
+            </figure>
+            <nav className="navbar">
+                <h4>CHARACTERS</h4>
+                <h4>COMICS</h4>
+                <h4>MOVIES</h4>
+                <h4>TV</h4>
+                <h4>GAMES</h4>
+                <h4>COLLECTIBLES</h4>
+                <h4>VIDEOS</h4>
+                <h4>FANS</h4>
+                <h4>NEWS</h4>
+                <h4>SHOP</h4>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/assets/components/Main.jsx
@@ -0,0 +1,11 @@
+const Main = () => {
+    return (
+        <main>
+            <div className="centratura-contenuto">
+                <p>Content goes here</p>
+            </div>
+        </main>
+    )
+}
+
+export default Main
